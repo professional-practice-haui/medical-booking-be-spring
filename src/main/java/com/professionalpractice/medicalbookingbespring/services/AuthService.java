7c7225bb040bd921af8bf8eb2728b227127a1900
caotrungduc <trungdc2003@gmail.com>
2024-04-17 22:09:20 +0700

    feat: format and ignore application-dev

--- src/main/java/com/professionalpractice/medicalbookingbespring/services/AuthService.java
+++ src/main/java/com/professionalpractice/medicalbookingbespring/services/AuthService.java
@@ -1,10 +1,9 @@
 package com.professionalpractice.medicalbookingbespring.services;
 
 import com.professionalpractice.medicalbookingbespring.dtos.LoginDto;
-import com.professionalpractice.medicalbookingbespring.dtos.UserDto;
 
 public interface AuthService {
 
-    String login (LoginDto body) throws Exception;
+    String login(LoginDto body);
 
 }
