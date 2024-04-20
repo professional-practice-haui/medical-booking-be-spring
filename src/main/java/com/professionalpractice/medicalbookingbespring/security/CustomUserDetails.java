7f43e9c740e33890d115a414334d7b48f29b068e
caotrungduc <trungdc2003@gmail.com>
2024-04-20 21:53:36 +0700

    feat: add role table

--- src/main/java/com/professionalpractice/medicalbookingbespring/security/CustomUserDetails.java
+++ src/main/java/com/professionalpractice/medicalbookingbespring/security/CustomUserDetails.java
@@ -21,40 +21,44 @@ public class CustomUserDetails implements UserDetails {
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         List<GrantedAuthority> roles = new ArrayList<>();
-        for (String role : user.getRoles()) {
-            roles.add(new SimpleGrantedAuthority("ROLE_" + role));
-        }
+        user.getRoles().forEach(role -> roles.add(new SimpleGrantedAuthority("ROLE_" + role.getRoleName().toUpperCase())));
         return roles;
     }
 
 
     @Override
     public String getPassword() {
+
         return user.getPassword();
     }
 
     @Override
     public String getUsername() {
+
         return user.getEmail();
     }
 
     @Override
     public boolean isAccountNonExpired() {
+
         return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
+
         return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
+
         return true;
     }
 
     @Override
     public boolean isEnabled() {
+
         return true;
     }
 }
