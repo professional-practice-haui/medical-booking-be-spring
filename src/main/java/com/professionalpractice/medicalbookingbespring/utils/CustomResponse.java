680ff527539c7e921098ebd171ef3c0b475bca38
caotrungduc <trungdc2003@gmail.com>
2024-04-21 18:58:38 +0700

    feat: register and login

--- src/main/java/com/professionalpractice/medicalbookingbespring/utils/CustomResponse.java
+++ src/main/java/com/professionalpractice/medicalbookingbespring/utils/CustomResponse.java
@@ -7,28 +7,33 @@ import org.springframework.util.MultiValueMap;
 
 public class CustomResponse {
 
-  public static ResponseEntity<RestData<?>> success(Object data) {
-    return success(HttpStatus.OK, data);
-  }
+    public static ResponseEntity<RestData<?>> success(Object data) {
 
-  public static ResponseEntity<RestData<?>> success(HttpStatus status, Object data) {
-    RestData<?> response = new RestData<>(status.value(), data);
-    return new ResponseEntity<>(response, status);
-  }
+        return success(HttpStatus.OK, "Thành công", data);
+    }
 
-  public static ResponseEntity<RestData<?>> success(MultiValueMap<String, String> header, Object data) {
-    return success(HttpStatus.OK, header, data);
-  }
+    public static ResponseEntity<RestData<?>> success(HttpStatus status, Object data) {
+        return success(status, "Thành công", data);
+    }
 
-  public static ResponseEntity<RestData<?>> success(HttpStatus status, MultiValueMap<String, String> header, Object data) {
-    RestData<?> response = new RestData<>(status.value(), data);
-    HttpHeaders responseHeaders = new HttpHeaders();
-    responseHeaders.addAll(header);
-    return ResponseEntity.ok().headers(responseHeaders).body(response);
-  }
+    public static ResponseEntity<RestData<?>> success(HttpStatus status, String message, Object data) {
+        RestData<?> response = new RestData<>(status.value(), message, data);
+        return new ResponseEntity<>(response, status);
+    }
 
-  public static ResponseEntity<RestData<?>> error(HttpStatus status, Object message) {
-    RestData<?> response = RestData.error(status.value(), message);
-    return new ResponseEntity<>(response, status);
-  }
+    public static ResponseEntity<RestData<?>> success(MultiValueMap<String, String> header, Object data) {
+        return success(HttpStatus.OK, header, data);
+    }
+
+    public static ResponseEntity<RestData<?>> success(HttpStatus status, MultiValueMap<String, String> header, Object data) {
+        RestData<?> response = new RestData<>(status.value(), data);
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.addAll(header);
+        return ResponseEntity.ok().headers(responseHeaders).body(response);
+    }
+
+    public static ResponseEntity<RestData<?>> error(HttpStatus status, Object message) {
+        RestData<?> response = RestData.error(status.value(), message);
+        return new ResponseEntity<>(response, status);
+    }
 }
