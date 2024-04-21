4640a53d1b1a6079bb580efd9f520b6c77ae3b06
caotrungduc <trungdc2003@gmail.com>
2024-04-21 19:33:12 +0700

    chá»ore: change some name

--- src/main/java/com/professionalpractice/medicalbookingbespring/dtos/LoginResponseDto.java
+++ src/main/java/com/professionalpractice/medicalbookingbespring/dtos/LoginResponseDto.java
@@ -12,7 +12,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class LoginResponseDto {
-    UserDto userDto;
+    UserDto user;
     List<String> roles;
     String token;
 }
