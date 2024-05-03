d475e1f27a7e2dea5db1960f4efe8ba4d6992d08
Duong MDat <duongmdat2k3@gmail.com>
2024-05-03 09:49:51 +0700

    feat: add doctor and department

--- src/main/java/com/professionalpractice/medicalbookingbespring/repositories/DepartmentRepository.java
+++ src/main/java/com/professionalpractice/medicalbookingbespring/repositories/DepartmentRepository.java
@@ -1,10 +1,16 @@
 package com.professionalpractice.medicalbookingbespring.repositories;
 
 import com.professionalpractice.medicalbookingbespring.entities.Department;
+import com.professionalpractice.medicalbookingbespring.entities.Doctor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
 
+    @Query("SELECT u FROM Department u")
+    Page<Department> queryDepartments(Pageable pageable);
 }
